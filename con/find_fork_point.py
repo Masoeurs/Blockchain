f916ee4544a6e55d71efbd0e9553c2c967a9fe99
Masoeurs Finance <161754933+Masoeurs@users.noreply.github.com>
2024-03-17 05:28:04 +0000

    Add files via upload

---
+++ con/find_fork_point.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+from typing import Dict, Tuple, Union
+
+from MSSF.consensus.block_record import BlockRecord
+from MSSF.consensus.blockchain_interface import BlockchainInterface
+from MSSF.types.blockchain_format.sized_bytes import bytes32
+from MSSF.types.header_block import HeaderBlock
+from MSSF.util.ints import uint32
+
+
+async def find_fork_point_in_chain(
+    blocks: BlockchainInterface,
+    block_1: Union[BlockRecord, HeaderBlock],
+    block_2: Union[BlockRecord, HeaderBlock],
+) -> int:
+    """Tries to find height where new chain (block_2) diverged from block_1 (assuming prev blocks
+    are all included in chain)
+    Returns -1 if chains have no common ancestor
+    * assumes the fork point is loaded in blocks
+    """
+    height_1 = int(block_1.height)
+    height_2 = int(block_2.height)
+    bh_1 = block_1.header_hash
+    bh_2 = block_2.header_hash
+
+    # special case for first level, since we actually already know the previous
+    # hash
+    if height_1 > height_2:
+        bh_1 = block_1.prev_hash
+        height_1 -= 1
+    elif height_2 > height_1:
+        bh_2 = block_2.prev_hash
+        height_2 -= 1
+
+    while height_1 > height_2:
+        [bh_1] = await blocks.prev_block_hash([bh_1])
+        height_1 -= 1
+
+    while height_2 > height_1:
+        [bh_2] = await blocks.prev_block_hash([bh_2])
+        height_2 -= 1
+
+    assert height_1 == height_2
+
+    height = height_2
+    while height > 0:
+        if bh_1 == bh_2:
+            return height
+        [bh_1, bh_2] = await blocks.prev_block_hash([bh_1, bh_2])
+        height -= 1
+
+    if bh_2 != bh_1:
+        # All blocks are different
+        return -1
+
+    # First block is the same
+    return 0
+
+
+async def lookup_fork_chain(
+    blocks: BlockchainInterface,
+    block_1: Tuple[uint32, bytes32],
+    block_2: Tuple[uint32, bytes32],
+) -> Tuple[Dict[uint32, bytes32], bytes32]:
+    """Tries to find height where new chain (block_2) diverged from block_1.
+    The inputs are (height, header-hash)-tuples.
+    Returns two values:
+        1. The height to hash map of block_2's chain down to, but not
+           including, the fork height
+        2. The header hash of the block at the fork height
+    """
+    height_1 = int(block_1[0])
+    bh_1 = block_1[1]
+    height_2 = int(block_2[0])
+    bh_2 = block_2[1]
+
+    ret: Dict[uint32, bytes32] = {}
+
+    while height_1 > height_2:
+        [bh_1] = await blocks.prev_block_hash([bh_1])
+        height_1 -= 1
+
+    while height_2 > height_1:
+        ret[uint32(height_2)] = bh_2
+        [bh_2] = await blocks.prev_block_hash([bh_2])
+        height_2 -= 1
+
+    assert height_1 == height_2
+
+    height = height_2
+    while height > 0:
+        if bh_1 == bh_2:
+            return (ret, bh_2)
+        ret[uint32(height)] = bh_2
+        [bh_1, bh_2] = await blocks.prev_block_hash([bh_1, bh_2])
+        height -= 1
+
+    if bh_1 == bh_2:
+        return (ret, bh_2)
+
+    ret[uint32(0)] = bh_2
+    # TODO: if we would pass in the consensus constants, we could return the
+    # GENESIS_CHALLENGE hash here, instead of zeros
+    return (ret, bytes32([0] * 32))
