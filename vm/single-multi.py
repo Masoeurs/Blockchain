9ec21dee03cea7372964c42fc83ac76eb9bd93b4
Masoeurs Finance <161754933+Masoeurs@users.noreply.github.com>
2024-03-21 00:35:06 +0000

    Update single-multi.py

--- vm/single-multi.py
+++ vm/single-multi.py
@@ -1,7 +1,3 @@
-"""
-Single Transaction / Multi Transactions
-"""
-
 import contextlib
 import json
 import random
@@ -21,7 +17,7 @@ SINGLETON_TOP_LAYER_MOD = load_clvm_maybe_recompile("singleton_top_layer.clsp")
 SINGLETON_LAUNCHER = load_clvm_maybe_recompile("singleton_launcher.clsp")
 from __future__ import annotations
 
-from mssf.consensus.block_rewards import calculate_base_farmer_reward, calculate_pool_reward
+from mssf.consensus.bl_rewards import calculate_base_farmer_reward, calculate_pool_reward
 from mssf.consensus.coinbase import create_farmer_coin, create_pool_coin
 from mssf.consensus.constants import ConsensusConstants
 from mssf.consensus.cost_calculator import NPCResult
@@ -32,12 +28,12 @@ from mssf.full_node.hint_store import HintStore
 from mssf.full_node.mempool import Mempool
 from mssf.full_node.mempool_check_conditions import get_name_puzzle_conditions, get_puzzle_and_solution_for_coin
 from mssf.full_node.mempool_manager import MempoolManager
-from mssf.types.blockchain_format.coin import Coin
-from mssf.types.blockchain_format.program import INFINITE_COST
-from mssf.types.blockchain_format.sized_bytes import bytes32
+from mssf.types.blchain_format.coin import Coin
+from mssf.types.blchain_format.program import INFINITE_COST
+from mssf.types.blchain_format.sized_bytes import bytes32
 from mssf.types.coin_record import CoinRecord
 from mssf.types.coin_spend import CoinSpend
-from mssf.types.generator_types import BlockGenerator
+from mssf.types.generator_types import blGenerator
 from mssf.types.mempool_inclusion_status import MempoolInclusionStatus
 from mssf.types.mempool_item import MempoolItem
 from mssf.types.spend_bundle import SpendBundle
@@ -54,7 +50,7 @@ async def sim_and_client(
     async with SpendSim.managed(db_path, defaults) as sim:
         client: SimClient = SimClient(sim)
         if pass_prefarm:
-            await sim.farm_block()
+            await sim.farm_bl()
         yield sim, client
 
 
@@ -64,7 +60,7 @@ class CostLogger:
         self.cost_dict_no_puzs: Dict[str, int] = {}
 
     def add_cost(self, descriptor: str, spend_bundle: SpendBundle) -> SpendBundle:
-        program: BlockGenerator = simple_solution_generator(spend_bundle)
+        program: blGenerator = simple_solution_generator(spend_bundle)
         npc_result: NPCResult = get_name_puzzle_conditions(
             program,
             INFINITE_COST,
@@ -90,36 +86,36 @@ class CostLogger:
 
 @streamable
 @dataclass(frozen=True)
-class SimFullBlock(Streamable):
-    transactions_generator: Optional[BlockGenerator]
-    height: uint32  # Note that height is not on a regular FullBlock
+class SimFullbl(Streamable):
+    transactions_generator: Optional[blGenerator]
+    height: uint32  # Note that height is not on a regular Fullbl
 
 
-_T_SimBlockRecord = TypeVar("_T_SimBlockRecord", bound="SimBlockRecord")
+_T_SimblRecord = TypeVar("_T_SimblRecord", bound="SimblRecord")
 
 
 @streamable
 @dataclass(frozen=True)
-class SimBlockRecord(Streamable):
+class SimblRecord(Streamable):
     reward_claims_incorporated: List[Coin]
     height: uint32
-    prev_transaction_block_height: uint32
+    prev_transaction_bl_height: uint32
     timestamp: uint64
-    is_transaction_block: bool
+    is_transaction_bl: bool
     header_hash: bytes32
-    prev_transaction_block_hash: bytes32
+    prev_transaction_bl_hash: bytes32
 
     @classmethod
-    def create(cls: Type[_T_SimBlockRecord], rci: List[Coin], height: uint32, timestamp: uint64) -> _T_SimBlockRecord:
-        prev_transaction_block_height = uint32(height - 1 if height > 0 else 0)
+    def create(cls: Type[_T_SimblRecord], rci: List[Coin], height: uint32, timestamp: uint64) -> _T_SimblRecord:
+        prev_transaction_bl_height = uint32(height - 1 if height > 0 else 0)
         return cls(
             rci,
             height,
-            prev_transaction_block_height,
+            prev_transaction_bl_height,
             timestamp,
             True,
             std_hash(height.stream_to_bytes()),
-            std_hash(prev_transaction_block_height.stream_to_bytes()),
+            std_hash(prev_transaction_bl_height.stream_to_bytes()),
         )
 
 
@@ -127,9 +123,9 @@ class SimBlockRecord(Streamable):
 @dataclass(frozen=True)
 class SimStore(Streamable):
     timestamp: uint64
-    block_height: uint32
-    block_records: List[SimBlockRecord]
-    blocks: List[SimFullBlock]
+    bl_height: uint32
+    bl_records: List[SimblRecord]
+    bls: List[SimFullbl]
 
 
 _T_SpendSim = TypeVar("_T_SpendSim", bound="SpendSim")
@@ -139,10 +135,10 @@ class SpendSim:
     db_wrapper: DBWrapper2
     coin_store: CoinStore
     mempool_manager: MempoolManager
-    block_records: List[SimBlockRecord]
-    blocks: List[SimFullBlock]
+    bl_records: List[SimblRecord]
+    bls: List[SimFullbl]
     timestamp: uint64
-    block_height: uint32
+    bl_height: uint32
     defaults: ConsensusConstants
     hint_store: HintStore
 
@@ -165,43 +161,43 @@ class SpendSim:
 
             # Load the next data if there is any
             async with self.db_wrapper.writer_maybe_transaction() as conn:
-                await conn.execute("CREATE TABLE IF NOT EXISTS block_data(data blob PRIMARY KEY)")
-                cursor = await conn.execute("SELECT * from block_data")
+                await conn.execute("CREATE TABLE IF NOT EXISTS bm_data(data blob PRIMARY KEY)")
+                cursor = await conn.execute("SELECT * from bm_data")
                 row = await cursor.fetchone()
                 await cursor.close()
                 if row is not None:
                     store_data = SimStore.from_bytes(row[0])
                     self.timestamp = store_data.timestamp
-                    self.block_height = store_data.block_height
-                    self.block_records = store_data.block_records
-                    self.blocks = store_data.blocks
-                    self.mempool_manager.peak = self.block_records[-1]
+                    self.bl_height = store_data.bl_height
+                    self.bl_records = store_data.bl_records
+                    self.bls = store_data.bls
+                    self.mempool_manager.peak = self.bl_records[-1]
                 else:
                     self.timestamp = uint64(1)
-                    self.block_height = uint32(0)
-                    self.block_records = []
-                    self.blocks = []
+                    self.bl_height = uint32(0)
+                    self.bl_records = []
+                    self.bls = []
 
             try:
                 yield self
             finally:
                 with anyio.CancelScope(shield=True):
                     async with self.db_wrapper.writer_maybe_transaction() as conn:
-                        c = await conn.execute("DELETE FROM block_data")
+                        c = await conn.execute("DELETE FROM bm_data")
                         await c.close()
                         c = await conn.execute(
-                            "INSERT INTO block_data VALUES(?)",
-                            (bytes(SimStore(self.timestamp, self.block_height, self.block_records, self.blocks)),),
+                            "INSERT INTO bm_data VALUES(?)",
+                            (bytes(SimStore(self.timestamp, self.bl_height, self.bl_records, self.bls)),),
                         )
                         await c.close()
 
     async def new_peak(self, spent_coins_ids: Optional[List[bytes32]]) -> None:
-        await self.mempool_manager.new_peak(self.block_records[-1], spent_coins_ids)
+        await self.mempool_manager.new_peak(self.bl_records[-1], spent_coins_ids)
 
     def new_coin_record(self, coin: Coin, coinbase: bool = False) -> CoinRecord:
         return CoinRecord(
             coin,
-            uint32(self.block_height + 1),
+            uint32(self.bl_height + 1),
             uint32(0),
             coinbase,
             self.timestamp,
@@ -221,12 +217,12 @@ class SpendSim:
             coins.add(coin)
         return list(coins)
 
-    async def generate_transaction_generator(self, bundle: Optional[SpendBundle]) -> Optional[BlockGenerator]:
+    async def generate_transaction_generator(self, bundle: Optional[SpendBundle]) -> Optional[blGenerator]:
         if bundle is None:
             return None
         return simple_solution_generator(bundle)
 
-    async def farm_block(
+    async def farm_bl(
         self,
         puzzle_hash: bytes32 = bytes32(b"0" * 32),
         item_inclusion_filter: Optional[Callable[[bytes32], bool]] = None,
@@ -237,17 +233,17 @@ class SpendSim:
             fees = uint64(fees + item.fee)
 
         # Rewards get created
-        next_block_height: uint32 = uint32(self.block_height + 1) if len(self.block_records) > 0 else self.block_height
+        next_bl_height: uint32 = uint32(self.bl_height + 1) if len(self.bl_records) > 0 else self.bl_height
         pool_coin: Coin = create_pool_coin(
-            next_block_height,
+            next_bl_height,
             puzzle_hash,
-            calculate_pool_reward(next_block_height),
+            calculate_pool_reward(next_bl_height),
             self.defaults.GENESIS_CHALLENGE,
         )
         farmer_coin: Coin = create_farmer_coin(
-            next_block_height,
+            next_bl_height,
             puzzle_hash,
-            uint64(calculate_base_farmer_reward(next_block_height) + fees),
+            uint64(calculate_base_farmer_reward(next_bl_height) + fees),
             self.defaults.GENESIS_CHALLENGE,
         )
         await self.coin_store._add_coin_records(
@@ -259,7 +255,7 @@ class SpendSim:
         return_additions: List[Coin] = []
         return_removals: List[Coin] = []
         spent_coins_ids = None
-        if (len(self.block_records) > 0) and (self.mempool_manager.mempool.size() > 0):
+        if (len(self.bl_records) > 0) and (self.mempool_manager.mempool.size() > 0):
             peak = self.mempool_manager.peak
             if peak is not None:
                 result = await self.mempool_manager.create_bundle_from_mempool(
@@ -283,21 +279,21 @@ class SpendSim:
                     return_removals = bundle.removals()
                     spent_coins_ids = [r.name() for r in return_removals]
                     await self.coin_store._add_coin_records([self.new_coin_record(addition) for addition in additions])
-                    await self.coin_store._set_spent(spent_coins_ids, uint32(self.block_height + 1))
+                    await self.coin_store._set_spent(spent_coins_ids, uint32(self.bl_height + 1))
 
-        # SimBlockRecord is created
-        generator: Optional[BlockGenerator] = await self.generate_transaction_generator(generator_bundle)
-        self.block_records.append(
-            SimBlockRecord.create(
+        # SimblRecord is created
+        generator: Optional[blGenerator] = await self.generate_transaction_generator(generator_bundle)
+        self.bl_records.append(
+            SimblRecord.create(
                 [pool_coin, farmer_coin],
-                next_block_height,
+                next_bl_height,
                 self.timestamp,
             )
         )
-        self.blocks.append(SimFullBlock(generator, next_block_height))
+        self.bls.append(SimFullbl(generator, next_bl_height))
 
-        # block_height is incremented
-        self.block_height = next_block_height
+        # bl_height is incremented
+        self.bl_height = next_bl_height
 
         # mempool is reset
         await self.new_peak(spent_coins_ids)
@@ -306,23 +302,23 @@ class SpendSim:
         return return_additions, return_removals
 
     def get_height(self) -> uint32:
-        return self.block_height
+        return self.bl_height
 
     def pass_time(self, time: uint64) -> None:
         self.timestamp = uint64(self.timestamp + time)
 
-    def pass_blocks(self, blocks: uint32) -> None:
-        self.block_height = uint32(self.block_height + blocks)
+    def pass_bls(self, bls: uint32) -> None:
+        self.bl_height = uint32(self.bl_height + bls)
 
-    async def rewind(self, block_height: uint32) -> None:
-        new_br_list = list(filter(lambda br: br.height <= block_height, self.block_records))
-        new_block_list = list(filter(lambda block: block.height <= block_height, self.blocks))
-        self.block_records = new_br_list
-        self.blocks = new_block_list
-        await self.coin_store.rollback_to_block(block_height)
+    async def rewind(self, bl_height: uint32) -> None:
+        new_br_list = list(filter(lambda br: br.height <= bl_height, self.bl_records))
+        new_bl_list = list(filter(lambda bl: bl.height <= bl_height, self.bls))
+        self.bl_records = new_br_list
+        self.bls = new_bl_list
+        await self.coin_store.rollback_to_bl(bl_height)
         old_pool = self.mempool_manager.mempool
         self.mempool_manager.mempool = Mempool(old_pool.mempool_info, old_pool.fee_estimator)
-        self.block_height = block_height
+        self.bl_height = bl_height
         if new_br_list:
             self.timestamp = new_br_list[-1].timestamp
         else:
@@ -406,39 +402,39 @@ class SimClient:
             kwargs["end_height"] = end_height
         return await self.service.coin_store.get_coin_records_by_puzzle_hashes(**kwargs)
 
-    async def get_block_record_by_height(self, height: uint32) -> SimBlockRecord:
-        return list(filter(lambda block: block.height == height, self.service.block_records))[0]
+    async def get_bl_record_by_height(self, height: uint32) -> SimblRecord:
+        return list(filter(lambda bl: bl.height == height, self.service.bl_records))[0]
 
-    async def get_block_record(self, header_hash: bytes32) -> SimBlockRecord:
-        return list(filter(lambda block: block.header_hash == header_hash, self.service.block_records))[0]
+    async def get_bl_record(self, header_hash: bytes32) -> SimblRecord:
+        return list(filter(lambda bl: bl.header_hash == header_hash, self.service.bl_records))[0]
 
-    async def get_block_records(self, start: uint32, end: uint32) -> List[SimBlockRecord]:
-        return list(filter(lambda block: (block.height >= start) and (block.height < end), self.service.block_records))
+    async def get_bl_records(self, start: uint32, end: uint32) -> List[SimblRecord]:
+        return list(filter(lambda bl: (bl.height >= start) and (bl.height < end), self.service.bl_records))
 
-    async def get_block(self, header_hash: bytes32) -> SimFullBlock:
-        selected_block: SimBlockRecord = list(
-            filter(lambda br: br.header_hash == header_hash, self.service.block_records)
+    async def get_bl(self, header_hash: bytes32) -> SimFullbl:
+        selected_bl: SimblRecord = list(
+            filter(lambda br: br.header_hash == header_hash, self.service.bl_records)
         )[0]
-        block_height: uint32 = selected_block.height
-        block: SimFullBlock = list(filter(lambda block: block.height == block_height, self.service.blocks))[0]
-        return block
+        bl_height: uint32 = selected_bl.height
+        bl: SimFullbl = list(filter(lambda bl: bl.height == bl_height, self.service.bls))[0]
+        return bl
 
-    async def get_all_block(self, start: uint32, end: uint32) -> List[SimFullBlock]:
-        return list(filter(lambda block: (block.height >= start) and (block.height < end), self.service.blocks))
+    async def get_all_bl(self, start: uint32, end: uint32) -> List[SimFullbl]:
+        return list(filter(lambda bl: (bl.height >= start) and (bl.height < end), self.service.bls))
 
     async def get_additions_and_removals(self, header_hash: bytes32) -> Tuple[List[CoinRecord], List[CoinRecord]]:
-        selected_block: SimBlockRecord = list(
-            filter(lambda br: br.header_hash == header_hash, self.service.block_records)
+        selected_bl: SimblRecord = list(
+            filter(lambda br: br.header_hash == header_hash, self.service.bl_records)
         )[0]
-        block_height: uint32 = selected_block.height
-        additions: List[CoinRecord] = await self.service.coin_store.get_coins_added_at_height(block_height)
-        removals: List[CoinRecord] = await self.service.coin_store.get_coins_removed_at_height(block_height)
+        bl_height: uint32 = selected_bl.height
+        additions: List[CoinRecord] = await self.service.coin_store.get_coins_added_at_height(bl_height)
+        removals: List[CoinRecord] = await self.service.coin_store.get_coins_removed_at_height(bl_height)
         return additions, removals
 
     async def get_puzzle_and_solution(self, coin_id: bytes32, height: uint32) -> CoinSpend:
-        filtered_generators = list(filter(lambda block: block.height == height, self.service.blocks))
+        filtered_generators = list(filter(lambda bl: bl.height == height, self.service.bls))
         # real consideration should be made for the None cases instead of just hint ignoring
-        generator: BlockGenerator = filtered_generators[0].transactions_generator  # type: ignore[assignment]
+        generator: blGenerator = filtered_generators[0].transactions_generator  # type: ignore[assignment]
         coin_record = await self.service.coin_store.get_coin_record(coin_id)
         assert coin_record is not None
         spend_info = get_puzzle_and_solution_for_coin(generator, coin_record.coin, height, self.service.defaults)
